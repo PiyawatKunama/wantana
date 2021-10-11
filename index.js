0d9c919b8e2fdaec87e3c1a08eb0b8c354f3b33d
Jimmy <jimmy.skuberg@gmail.com>
2021-10-12 01:15:40 +0700

    proxy

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ app.post("/webhook", function (req, res) {
 				redirect: "follow",
 			};
 
-			fetch("http://localhost:3000/graphql", requestOptions)
+			fetch("http://127.0.0.1:3000/graphql", requestOptions)
 				.then((response) => response.text())
 				.then((result) => console.log(result))
 				.catch((error) => console.log("error", error));
